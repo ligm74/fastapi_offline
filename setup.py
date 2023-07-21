591cf01ae65e571730d02115eda06c6688cc1d93
ligm74 <57537447+ligm74@users.noreply.github.com>
2023-07-21 13:13:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,6 +56,5 @@ setup(
     tests_require=TEST_DEPS,
     setup_requires=[FASTAPI_VER],
     extras_require={"test": TEST_DEPS},
-    cmdclass={"install": SDistWrapper,
-              "develop": SDistWrapper}
+    cmdclass={"install": SDistWrapper}
 )
